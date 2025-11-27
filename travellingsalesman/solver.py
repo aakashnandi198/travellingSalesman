aaad07075785620fd67f49a51d0e097133e29d4c
Aakash <aakashnandi198@gmail.com>
2025-11-26 21:10:38 -0500

    Add branch and depth bounds

--- travellingsalesman/solver.py
+++ travellingsalesman/solver.py
@@ -5,15 +5,17 @@ import random
 from tqdm import tqdm
 import numpy as np
 
-MAX_ITERATIONS = 10**6
-MAX_SEEDING_TRIALS = 10**5
+MAX_SEEDING_TRIALS = 10**3
 
 class Solver:
     def __init__(self, points: namedtuple):
         self.points = points
         self.node_count = len(points)
+        self.node_mem = []
+        self.max_depth = 5
+        self.max_branch = 10
         self.tabu={}
-        self.tabu_limit = 2
+        self.tabu_limit=2
 
     def length(self, point1: namedtuple, point2: namedtuple) -> float:
         return ((point1.x - point2.x) ** 2 + (point1.y - point2.y) ** 2) ** 0.5
@@ -50,27 +52,27 @@ class Solver:
         return solution
     
     def select_seed_node(self, current_solution: list, itr : int) -> int:
-        # Randome select a node as seed for k-opt that is not in tabu list
-        candidates = [
-            node for node in current_solution
-            if node not in self.tabu or itr - self.tabu[node] > self.tabu_limit
-        ]
-        selected_node = random.choice(candidates)
-        self.tabu[selected_node] = itr
+        # Select a random node from the current solution as seed for k-opt based on iteration and tabu list
+        candidates = [node for node in current_solution if node not in self.tabu or itr - self.tabu[node] > self.tabu_limit]
+        if not candidates:
+            selected_node = -1
+        else:
+            selected_node = random.choice(candidates)
         return selected_node
         
     
-    def select_candidate(self, current_solution: list, point: int, itr :int, level:int) -> int:
+    def select_candidate(self, current_solution: list, point: int, itr: int, level:int) -> int:
         # Select a candidate node for k-opt swap
         # todo : improve selection strategy
         idx = current_solution.index(point)
         # the next idx is the current child, previous idx is current parent
         curr_child = current_solution[idx+1] if (idx+1)<len(current_solution) else current_solution[0]
         curr_parent = current_solution[idx-1]
+        node_count = len(current_solution)
         candidate_child = -1
         candidates = [
             node for node in current_solution
-            if node != point and node != curr_child and node != curr_parent and (node not in self.tabu or itr - self.tabu[node] > self.tabu_limit)
+            if node != point and node != curr_child and node != curr_parent and (node not in self.node_mem) and (node not in self.tabu or itr - self.tabu[node] > self.tabu_limit)
         ]
         if not candidates:
             candidate_child = -1
@@ -79,40 +81,71 @@ class Solver:
             # Make shorter distances more likely as iterations increase
             # Use a softmax-like probability, with a temperature that decreases over iterations
             # Higher temperature = more random, lower = more greedy
-            temperature = max(0.1, 10.0 / float(1 + level / 10000))
+            temperature = max(0.1, 10.0 / float(1 + level / node_count**2))
             inv_distances = np.exp(-np.array(distances) / temperature)
             probabilities = inv_distances / inv_distances.sum()
             candidate_child = random.choices(candidates, weights=probabilities, k=1)[0]
-        self.tabu[candidate_child] = itr
         return candidate_child
 
-    def k_optimize(self, current_solution: list, selected_node: int , itr :int, level : int) -> list:
-        self.tabu[selected_node] = itr
-        # select a candidate node to swap with
-        candidate = self.select_candidate(current_solution, selected_node, itr, level)
-        if candidate == -1:
-            return current_solution
-        # rotated solution to start from point
-        idx_selected_node = current_solution.index(selected_node)
-        rotated_solution = current_solution[idx_selected_node:] + current_solution[:idx_selected_node]
-        idx_candidate = rotated_solution.index(candidate)
-        candidate_solution = rotated_solution[:1] + rotated_solution[idx_candidate:0:-1]+ rotated_solution[idx_candidate+1:]
-        # rotate back to original starting point
-        idx_start = candidate_solution.index(current_solution[0])
-        candidate_solution = candidate_solution[idx_start:] + candidate_solution[:idx_start]
+    def k_optimize(self, current_solution: list, selected_node: int , itr ,level : int) -> list:
+        # print(f'K-opt level {level}, selected node {selected_node}')
+        # print self.node_mem
+        # print(f'nodes_in_memory: {self.node_mem}')
+        # set current value and best value
+        best_solution = current_solution
+        best_value = self.objective(current_solution)
         
-        # check if candidate solution can be improved
-        candidate_solution = self.k_optimize(candidate_solution, candidate, itr, level+1)
-
-        candidate_value = self.objective(candidate_solution)
-        current_value = self.objective(current_solution)
-
-        if candidate_value < current_value:
-            return candidate_solution
-        else:
-            del self.tabu[candidate]
-            #print("No improvement at level ", level)
+        # if maximum depth reached, return current solution
+        if level >= self.max_depth:
             return current_solution
+        
+        # add selected node to memory to avoid re-selection in this k-opt
+        self.node_mem.append(selected_node)
+        
+        # select a candidate node to swap with
+        # reduce the branch factor as level increases expontentially
+        for branch in range(self.max_branch // (2**(level-1))):
+            candidate = self.select_candidate(current_solution, selected_node, itr ,level)
+            if candidate == -1:
+                continue
+            
+            # add candidate to memory to avoid re-selection in this k-opt
+            self.node_mem.append(candidate)
+            # if there is tabu value for candidate store for reassigning later
+            # prev_tabu = self.tabu.get(candidate, None)
+            # set tabu for candidate
+            # self.tabu[candidate] = itr
+            
+            # rotated solution to start from point
+            idx_selected_node = current_solution.index(selected_node)
+            rotated_solution = current_solution[idx_selected_node:] + current_solution[:idx_selected_node]
+            idx_candidate = rotated_solution.index(candidate)
+            
+            # generate candidate solution by reversing the segment between selected_node and candidate
+            candidate_solution = rotated_solution[:1] + rotated_solution[idx_candidate:0:-1]+ rotated_solution[idx_candidate+1:]
+            
+            # rotate back to original starting point
+            idx_start = candidate_solution.index(current_solution[0])
+            candidate_solution = candidate_solution[idx_start:] + candidate_solution[:idx_start]
+        
+            # check if candidate solution can be improved
+            candidate_solution = self.k_optimize(candidate_solution, candidate, itr, level+1)
+            candidate_value = self.objective(candidate_solution)
+        
+            if candidate_value < best_value:
+                best_solution = candidate_solution
+                best_value = candidate_value
+
+            # remove candidate from memory after processing
+            self.node_mem.remove(candidate)
+            # restore previous tabu value for candidate
+            #if prev_tabu is not None:
+            #    self.tabu[candidate] = prev_tabu
+
+        # remove selected node from memory before returning    
+        self.node_mem.remove(selected_node)
+        # return the best solution found
+        return best_solution
     
     def optimal_solution(self) -> list:
         best_solution = self.greedy_solution()
@@ -120,11 +153,16 @@ class Solver:
         for itr in tqdm(range(MAX_SEEDING_TRIALS)):
             # chose a random index for k-opt
             selected_node = self.select_seed_node(best_solution, itr)
-            improved_solution = self.k_optimize(best_solution, selected_node, itr, 0)
+            if selected_node == -1:
+                continue
+            improved_solution = self.k_optimize(best_solution, selected_node, itr, 1)
             improved_solution_value = self.objective(improved_solution)
             if improved_solution_value < best_solution_value:
                 best_solution = improved_solution
                 best_solution_value = improved_solution_value
+                # set tabu for nodes in best solution
+                for node in best_solution:
+                    self.tabu[node] = itr
             #self.visualize(improved_solution)
         return best_solution
 
